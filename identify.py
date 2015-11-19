f7c0e8ce77954be31e7cb89ed443e89e596b4a83
Sean Smith <swsmith@bu.edu>
2015-11-18 20:42:47 -0500

    God isn't dead, he just couldn't find a parking place.

--- identify.py
+++ identify.py
@@ -4,6 +4,10 @@ from pybrain.tools.shortcuts     import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.structure.modules   import SoftmaxLayer
 
+from pylab import ion, ioff, figure, draw, contourf, clf, show, hold, plot
+from scipy import diag, arange, meshgrid, where
+from numpy.random import multivariate_normal
+
 
 import numpy as np
 import cv2
@@ -13,16 +17,33 @@ class Recognize:
 
 	def __init__(self):
 		self.net = self.buildNet()
-		people = []
-		for i in range(1, 41):
-			people.append('Person'+str(i))
-		self.classify = ClassificationDataSet(10304, nb_classes=40, class_labels=people)
+		self.classify = self.classify()
+		
 
 	# This call returns a network that has two inputs, three hidden and a single output neuron. 
 	# In PyBrain, these layers are Module objects and they are already connected with FullConnection objects.	
 	def buildNet(self):
 		print "Building a network..."
 		return buildNetwork(10304, 3, 40)
+
+	def classify(self):
+		people = []
+		for i in range(1, 41):
+			people.append('Person'+str(i))
+		self.classify = ClassificationDataSet(10304, nb_classes=40, class_labels=people)
+
+		tstdata, trndata = self.classify.splitWithProportion( 0.30 )
+		for n in xrange(0, tstdata.getLength()):
+			tstdata.addSample( tstdata.getSample(n)[0], tstdata.getSample(n)[1] )
+		for n in xrange(0, trndata.getLength()):
+			trndata.addSample( trndata.getSample(n)[0], trndata.getSample(n)[1] )
+
+		print "Number of training patterns: ", len(trndata)
+		print "Input and output dimensions: ", trndata.indim, trndata.outdim
+		print "First sample (input, target, class):"
+		print trndata['input'][0], trndata['target'][0]
+		return trndata
+
 
 	def activate(self, img):
 		print "Activating a network..."
@@ -31,16 +52,22 @@ class Recognize:
 		return i
 
 	def trainer(self):
+		print "self.net.outdim = "+str(self.net.outdim)
+		print "self.classify.outdim = "+str(self.classify.outdim)
+
+		
+
 		trainer = BackpropTrainer( self.net, dataset=self.classify, momentum=0.1, verbose=True, weightdecay=0.01)
-		for i in range(20):
-			trainer.trainEpochs( 1 )
+		#t.
+		# for i in range(20):
+		# 	trainer.trainEpochs( 1 )
+		# 	trnresult = percentError( trainer.testOnClassData(), trndata['class'])
+		# 	tstresult = percentError( trainer.testOnClassData( dataset=tstdata ), tstdata['class'] )
 
-
-	def train(self, img, num):
-		print "Training...", "Person = "+str(num)
-		i = self.classify.addSample(img.flatten(), [num])
-		print i
-		return i
+		# 	print "epoch: %4d" % trainer.totalepochs, \
+		# 		"  train error: %5.2f%%" % trnresult, \
+		# 		"  test error: %5.2f%%" % tstresult
+		
 
 	def train_images(self, start, stop, num_images):
 		# Load a person and dsiplay
@@ -50,11 +77,10 @@ class Recognize:
 			for num in range(1,num_images + 1):
 				img = cv2.imread('faces/s'+str(i)+'/'+str(num)+'.pgm', 0)
 				height, width = img.shape
-				print "height = "+str(height)+" width = "+str(width)
+				# print "height = "+str(height)+" width = "+str(width)
+				print "Training...", "Person = "+str(num)
+				self.classify.addSample(img.flatten(), [i])
 
-
-				# Train the neural net
-				self.train(img, i)
 
 				# cv2.namedWindow('image', cv2.WINDOW_NORMAL)
 				# cv2.imshow('image',img)
@@ -70,12 +96,13 @@ class Recognize:
 		return reduce(lambda i,x,j,y: i if x > y else j, enumerate(list))
 
 	def neuron(self, edges, input):
-		return np.dot(edges.transpose() , input)
+		return np.dot(edges.transpose(), input)
 
 	def main(self):
 
-		result = self.train_images(1, 5, 7)
+		result = self.train_images(1, 40, 7)
 		print result
+		self.trainer()
 
 		# edges = np.array([[-1.5, -.5], [1, 1],[1, 1]])
 		# input = np.array([[1], [0], [1]])
