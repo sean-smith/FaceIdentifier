da7e03b5739e54e2236a81df3d0bbb22d6c327d2
Sean Matuszak <sjmman94@wireless1x-155-41-109-226.bu.edu>
2015-12-06 15:49:18 -0500

    added the confusion matrix generator (not finished)

---
+++ confusionMatrixGenerator.py
@@ -0,0 +1,8 @@
+#/usr/bin/python
+#takes two argumaents (other than the name of this file), the name of the results file, and the number of classes
+#The results file should be tab delimited in the format: TRUE_CLASS_NUMBER, PREDICTED_CLASS_NUMBER
+import sys
+
+results =open(sys.argv[1])
+ 
+
