a77673dac21434cf3fc8c92994e467639fc9779e
Sean Matuszak <sjmman94@wireless1x-155-41-109-226.bu.edu>
2015-12-06 16:52:11 -0500

    Finished confusion matrix generator

--- confusionMatrixGenerator.py
+++ confusionMatrixGenerator.py
@@ -3,6 +3,52 @@
 #The results file should be tab delimited in the format: TRUE_CLASS_NUMBER, PREDICTED_CLASS_NUMBER
 import sys
 
-results =open(sys.argv[1])
- 
+#helper functions
+def generateZeroArray(length):
+	list = []
+	for i in range(length):
+		list.append(0)
+	return list
 
+
+
+#arguments
+results =open(sys.argv[1])
+numClasses = int(sys.argv[2])
+predictionPairs = {}
+#determining results
+for line in results:
+	fields = line.split("\t")
+	trueNumber = int(fields[0])
+	predictedNumber = int(fields[1])
+	if trueNumber in  predictionPairs:
+		valList = predictionPairs.get(trueNumber);
+		valList.append(predictedNumber)
+		#append to the array
+	else:
+		predictionPairs[trueNumber] = [predictedNumber]
+		#create key-val pair with an array
+
+#Accumulate output in csv format
+csv = ""
+for i in range(numClasses):
+	#get an empty array of size numClasses
+	values = generateZeroArray(numClasses)
+	#get the true number's values
+	listOfPredictions = predictionPairs.get(i)
+	#increase the values array at the corect index for each prediciton
+	for j in range(len(listOfPredictions)):
+		prediction = listOfPredictions[j]
+		oldVal = values[prediction]
+		newVal = oldVal + 1
+		values[prediction] = newVal
+
+	for val in values:
+		csv += str(val)
+		csv += ","
+		#note - results in a hanging comma
+	csv += "\n"
+
+print csv
+	#use values array to generate the csv format
+	
