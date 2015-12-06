eff2f8e55d300a87006a04a1a14ae48f0563591e
David Lavy <david.lavy88@gmail.com>
2015-12-06 16:40:42 -0500

    Inside folder

--- faces2/renameFaces.py
+++ faces2/renameFaces.py
@@ -1,3 +1,42 @@
+# import cv2
+
+# # rectangle color and lineWidth
+# color = (0, 0, 255)
+# lineWidth = 1
+
+# windowName = "Object Detection"
+
+# # load detection file (various files for different views and uses)
+# cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+
+# indexes = [1, 2, 4, 5, 6, 7, 8, 9, 11, 12]
+
+# for i in range(0, 10):
+#     # load an image to search for faces
+#     idx = '0' + str(indexes[i])
+#     if indexes[i] < 10:
+#         idx = '00' + idx
+#     elif (i >= 10 and i < 100):
+#         idx = '0' + idx
+#     face = "./faces2/image_" + idx + ".jpg"
+#     print face
+#     img = cv2.imread(face)
+
+#     # detect objects, return as list
+#     rects = cascade.detectMultiScale(img)
+#     print rects
+
+#     for x,y, width,height in rects:
+#         cv2.rectangle(img, (x,y), (x+width, y+height), color, lineWidth)
+#         roi  = img[y:(y+height), x:(x+width)]
+#         gray_image = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+#         gray_image = cv2.resize(gray_image, (150, 150))
+#         # equ = cv2.equalizeHist(gray_image)
+#     # cropped = './cropped/image_' + idx + '.jpg'
+#     # cv2.imwrite(cropped,gray_image)
+#     # cropped2 = './cropped2/image_' + idx + '.jpg'
+#     # cv2.imwrite(cropped2,equ)
+
 import os
 import shutil
 def copy_rename(old_file_name, new_file_name):
@@ -17,17 +56,17 @@ def copy_rename(old_file_name, new_file_name):
 
 
 def main():
-    indexes = [217, 218, 221, 222, 223, 225, 226, 230, 231, 234]
+    indexes = [i for i in range(242, 251+1)]
 
     for i in range(0, 10):
         # load an image to search for faces
         idx = '0' + str(indexes[i])
         if indexes[i] < 10:
             idx = '00' + idx
-        elif (indexes[i] >= 10 and indexes[i] < 100):
+        elif (i >= 10 and i < 100):
             idx = '0' + idx
-        face = "image_" + idx + ".jpg"
-        new_face = '10_0' + str(i) + ".jpg"
+        face = "faces2/image_" + idx + ".jpg"
+        new_face = '11_0' + str(i) + ".jpg"
         copy_rename(face,new_face)
 
 if __name__ == "__main__":
